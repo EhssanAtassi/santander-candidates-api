676c5ad7f10df9d0d6372424631a8bfe48d86f1f
Ehsan Alatassi <ihssan.alatassi@gmail.com>
2025-06-28 12:59:39 +0300

    update the database

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,10 +2,15 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Candidate } from '../candidates/entities/candidate.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'sqlite',
-  database: process.env.DATABASE_PATH || 'candidates.db',
+  // type: 'sqlite',
+  // database: process.env.DATABASE_PATH || 'candidates.db',
+
+  type: 'postgres',
+
+  url: process.env.DATABASE_URL,
   entities: [Candidate],
-  synchronize: process.env.DATABASE_SYNC === 'true' || process.env.NODE_ENV !== 'production',
-  logging: process.env.DATABASE_LOGGING === 'true' || process.env.NODE_ENV === 'development',
+  synchronize: process.env.NODE_ENV !== 'production',
+  logging: process.env.NODE_ENV === 'development',
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   autoLoadEntities: true,
 };
